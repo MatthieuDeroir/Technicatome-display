e134c96032fa40204450f03333c6ff5acc1dc05d
MatthieuDeroir <matthieu.deroir@epsi.fr>
2024-01-04 16:55:12 +0100

    add: debug on App

--- display/src/App.js
+++ display/src/App.js
@@ -61,6 +61,7 @@ function App() {
       console.log("dataRes", dataRes);
       console.log("dataRes.data", dataRes.data);
         console.log("dataRes.data[0]", dataRes.data[0]);
+        console.log("dataRes.data[0].temperature", dataRes.data[0].temperature);
       setTemperature(dataRes.data[0].temperature);
       console.log("temperature", temperature);
       setIsVeilleMode(checkIsInVeillePeriod(veilleRes[0]));
