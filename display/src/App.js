4bbc3f8561dce8e11889864afb953c947ea22bf8
GuillaumeLeGoff <legoff.guill@gmail.com>
2024-01-08 11:23:28 +0100

    Fix bug in login functionality

--- display/src/App.js
+++ display/src/App.js
@@ -1,209 +1,169 @@
-import React, {useEffect, useState} from "react";
-
-import {slideshowService} from "./services/SlideshowService";
-
+import React, { useEffect, useState } from "react";
+import { slideshowService } from "./services/SlideshowService";
 import _ from "lodash";
-
 import MediasPage from "./pages/MediasPage";
-import {slideshowStatutsService} from "./services/SlideshowStatutsService";
+import { slideshowStatutsService } from "./services/SlideshowStatutsService";
 import "./Global.css";
 import TestPage from "./pages/TestPage";
-import {settingsService} from "./services/SettingsService";
-import {accidentService} from "./services/AccidentServices";
-import {dataService} from "./services/DataService";
+import { settingsService } from "./services/SettingsService";
+import { accidentService } from "./services/AccidentServices";
+import { dataService } from "./services/DataService";
 import AccidentPage from "./pages/AccidentPage";
 import DataPage from "./pages/DataPage";
 
 function App() {
+  const [accident, setAccident] = useState({});
+  const [isVeilleMode, setIsVeilleMode] = useState(false);
+  const [isTesting, setIsTesting] = useState(false);
+  const [currentSlideshow, setCurrentSlideshow] = useState({});
+  const [currentMediaIndex, setCurrentMediaIndex] = useState(0);
 
-    const [accident, setAccident] = useState({});
-    const [isVeilleMode, setIsVeilleMode] = useState(false);
-    const [isTesting, setIsTesting] = useState(false);
-    const [currentSlideshow, setCurrentSlideshow] = useState({});
-    const [currentMediaIndex, setCurrentMediaIndex] = useState(0);
+  const [pageIndex, setPageIndex] = useState(0);
 
-    const [pageIndex, setPageIndex] = useState(0);
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [temperature, setTemperature] = useState("");
 
-    const [date, setDate] = useState("");
-    const [time, setTime] = useState("");
-    const [temperature, setTemperature] = useState("");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      setDate(now.toLocaleDateString());
+      setTime(
+        now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })
+      );
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPageIndex((prevIndex) => 1 - prevIndex);
+    }, 10000);
+    return () => clearInterval(interval);
+  }, []);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            const now = new Date();
-            setDate(now.toLocaleDateString());
-            setTime(
-                now.toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"})
-            ); // Updated this line
-        }, 1000);
-        return () => {
-            clearInterval(interval);
-        };
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const [accidentRes, veilleRes, slideshowRes, slideshowStatusRes, dataRes] =
+        await Promise.all([
+          accidentService.getAccident(),
+          settingsService.getSettings(),
+          slideshowService.getSlideshow(),
+          slideshowStatutsService.getSlideshowStatus(),
+        ]);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setPageIndex((prevIndex) => 1 - prevIndex);
-        }, 10000);
-        return () => clearInterval(interval);
-    }, []);
+      setIsVeilleMode(checkIsInVeillePeriod(veilleRes[0]));
+      setAccident(accidentRes[0]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const [accidentRes, veilleRes, slideshowRes, slideshowStatusRes, dataRes] =
-                await Promise.all([
-                    accidentService.getAccident(),
-                    settingsService.getSettings(),
-                    slideshowService.getSlideshow(),
-                    slideshowStatutsService.getSlideshowStatus(),
-                ]);
-
-
-            setIsVeilleMode(checkIsInVeillePeriod(veilleRes[0]));
-            setAccident(accidentRes[0]);
-            console.log("accident", accidentRes[0]);
-
-            const currentSlideshowId = slideshowStatusRes[0]?.slideshowId;
-            if (slideshowStatusRes[0]?.isRunning) {
-                const foundSlideshow = slideshowRes.data.slideshows.find(
-                    (slideshow) => slideshow._id === currentSlideshowId
-                );
-
-                // Vérifie si le diaporama actuel est le même que le précédent
-                if (!_.isEqual(currentSlideshow, foundSlideshow)) {
-                    setCurrentSlideshow(foundSlideshow);
-                    setCurrentMediaIndex(0);
-                }
-            } else if (!_.isEmpty(currentSlideshow)) {
-                // Si le diaporama n'est plus en cours, réinitialise currentSlideshow
-                setCurrentSlideshow({});
-            }
-            if (slideshowStatusRes[0]?.isTesting) {
-                setIsTesting(true);
-            } else {
-                setIsTesting(false);
-            }
-        };
-        fetchData();
-        const interval = setInterval(fetchData, 5000);
-        return () => clearInterval(interval);
-    }, [currentSlideshow]);
-
-    useEffect(() => {
-        const mediaInterval = setInterval(
-            () => {
-                setCurrentMediaIndex(
-                    (prevIndex) => (prevIndex + 1) % (currentSlideshow.media?.length || 1)
-                );
-            },
-            currentSlideshow.media && currentSlideshow.media.length > 0
-                ? currentSlideshow.media[currentMediaIndex]?.duration * 1000
-                : 5000
+      const currentSlideshowId = slideshowStatusRes[0]?.slideshowId;
+      if (slideshowStatusRes[0]?.isRunning) {
+        const foundSlideshow = slideshowRes.data.slideshows.find(
+          (slideshow) => slideshow._id === currentSlideshowId
         );
 
-        return () => clearInterval(mediaInterval);
-    }, [currentSlideshow, currentMediaIndex]);
-
-    const checkIsInVeillePeriod = (veilleData) => {
-        if (!veilleData.enable) {
-            return false;
+        if (!_.isEqual(currentSlideshow, foundSlideshow)) {
+          setCurrentSlideshow(foundSlideshow);
+          setCurrentMediaIndex(0);
         }
-        const currentHour = new Date().getHours();
-        const startHour = parseInt(veilleData.start.split(":")[0], 10);
-        const stopHour = parseInt(veilleData.stop.split(":")[0], 10);
+      } else if (!_.isEmpty(currentSlideshow)) {
+        setCurrentSlideshow({});
+      }
 
-        return currentHour >= startHour && stopHour <= currentHour;
+      if (slideshowStatusRes[0]?.isTesting) {
+        setIsTesting(true);
+      } else {
+        setIsTesting(false);
+      }
     };
+    fetchData();
+    const interval = setInterval(fetchData, 5000);
+    return () => clearInterval(interval);
+  }, [currentSlideshow]);
 
-
-
-    useEffect(() => {
-        const fetchTemperature = async () => {
-            const dataRes = await dataService.getData();
-            setTemperature(dataRes.data[0].temperature);
-            console.log("temperature", temperature)
-        };
-        fetchTemperature();
-        const interval = setInterval(fetchTemperature, 5000);
-        return () => clearInterval(interval);
-    });
+  useEffect(() => {
+    const mediaInterval = setInterval(
+      () => {
+        setCurrentMediaIndex(
+          (prevIndex) => (prevIndex + 1) % (currentSlideshow.media?.length || 1)
+        );
+      },
+      currentSlideshow.media && currentSlideshow.media.length > 0
+        ? currentSlideshow.media[currentMediaIndex]?.duration * 1000
+        : 5000
+    );
 
     return () => clearInterval(mediaInterval);
   }, [currentSlideshow, currentMediaIndex]);
 
   const checkIsInVeillePeriod = (veilleData) => {
+    console.log("veilleData", veilleData);
     if (!veilleData.enable) {
-      return false;
+      return true;
     }
     const currentHour = new Date().getHours();
     const startHour = parseInt(veilleData.start.split(":")[0], 10);
     const stopHour = parseInt(veilleData.stop.split(":")[0], 10);
-    return currentHour >= startHour && currentHour <= stopHour;
+
+    return currentHour >= startHour && currentHour < stopHour;
   };
 
+  useEffect(() => {
+    const fetchTemperature = async () => {
+      const dataRes = await dataService.getData();
+      setTemperature(dataRes.data[0].temperature);
+    };
+    fetchTemperature();
+    const interval = setInterval(fetchTemperature, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div
-      style={{
-        maxHeight: `${process.env.REACT_APP_HEIGHT}px`,
-        maxWidth: `${process.env.REACT_APP_WIDTH}px`,
-        overflow: "hidden",
-      }}
-    >
-      <img
-        style={{ width: "100%" , marginBottom:"-3px" }}
-        src="/HeaderPicture.png"
-        alt="logo"
-      />
-     
-      {isTesting ? (
-        <TestPage />
-      ) : !isVeilleMode ? (
-        <></>
-      ) : currentSlideshow.media && currentSlideshow.media.length > 0 ? (
-        currentSlideshow.media.map((media, index) => (
+    
           <div
-            key={media._id}
             style={{
-                maxHeight: `${process.env.REACT_APP_HEIGHT}px`,
-                maxWidth: `${process.env.REACT_APP_WIDTH}px`,
-                overflow: "hidden",
+              maxHeight: `${process.env.REACT_APP_HEIGHT}px`,
+              maxWidth: `${process.env.REACT_APP_WIDTH}px`,
+              overflow: "hidden",
             }}
-        >
+          >
             <img
-                style={{width: "100%"}}
-                src="/HeaderPicture.png"
-                alt="logo"
-                className="logo"
+              style={{ width: "100%", marginBottom:"-3px" }}
+              src="/HeaderPicture.png"
+              alt="logo"
+              className="logo"
             />
             {isTesting ? (
-                <TestPage/>
-            ) : isVeilleMode ? (
-                <></>
+              <TestPage />
+            ) : !isVeilleMode ? (
+              <></>
             ) : currentSlideshow.media && currentSlideshow.media.length > 0 ? (
-                currentSlideshow.media.map((media, index) => (
-                    <div
-                        key={media._id}
-                        style={{
-                            display: index === currentMediaIndex ? "block" : "none",
-                        }}
-                    >
-                        {media.type === "Panneau" ? (
-                            <AccidentPage accident={accident}/>
-                        ) : media.type === "Data" ? (
-                            <DataPage time={time} date={date} temperature={temperature}/>
-                        ) : (
-                            <MediasPage media={media}/>
-                        )}
-                    </div>
-                ))
+              currentSlideshow.media.map((media, index) => (
+                <div
+                  key={media._id}
+                  style={{
+                    display: index === currentMediaIndex ? "block" : "none",
+                  }}
+                >
+                  {media.type === "Panneau" ? (
+                    <AccidentPage accident={accident} />
+                  ) : media.type === "Data" ? (
+                    <DataPage time={time} date={date} temperature={temperature} />
+                  ) : (
+                    <MediasPage media={media} />
+                  )}
+                </div>
+              ))
             ) : pageIndex === 0 ? (
-                <AccidentPage accident={accident}/>
+              <AccidentPage accident={accident} />
             ) : (
-                <DataPage time={time} date={date} temperature={temperature}/>
+              <DataPage time={time} date={date} temperature={temperature} />
             )}
-        </div>
-    );
+          </div>
+        
+ 
+  );
 }
 
 export default App;
